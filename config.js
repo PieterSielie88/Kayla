64ae2239ccc078842353252f28f4471cc5ab738a
PieterSielie88 <johan.richards@icloud.com>
2026-02-05 20:42:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/di0ssxhbz/video/upload/v1770316778/YTDown.com_YouTube_Mark-Ambor-Belong-Together-Official-Lyri_Media_xPWnNFF-TAw_001_1080p_oubwtd.mp4", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
